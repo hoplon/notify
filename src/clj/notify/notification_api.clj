2fe474dbbcd02e7a9db295959d19ca64a1b13e10
Bill La Forge <laforge49@gmail.com>
2016-01-08 14:31:24 -0500

    notify rewrite works with random

--- src/clj/notify/notification_api.clj
+++ src/clj/notify/notification_api.clj
@@ -13,17 +13,17 @@
   (assoc pm session-id [timestamp ack notifications]))
 (defn sessions-count [] (count @aged-sessions))
 (defn get-timestamp [pm session-id]
-  (get-in pm [0 session-id]))
+  (get-in pm [session-id 0]))
 (defn assoc-timestamp [pm session-id timestamp]
-  (assoc-in pm [0 session-id] timestamp))
-(defn get-ack [pm session-id]
-  (get-in pm [1 session-id]))
-(defn assoc-ack [pm session-id ack]
-  (assoc-in pm [1 session-id] ack))
+  (assoc-in pm [session-id 0] timestamp))
+(defn get-last-sent [pm session-id]
+  (get-in pm [session-id 1]))
+(defn assoc-last-sent [pm session-id ack]
+  (assoc-in pm [session-id 1] ack))
 (defn get-unacked-notifications [pm session-id]
-  (get-in pm [2 session-id]))
+  (get-in pm [session-id 2]))
 (defn assoc-unacked-notifications [pm session-id notifications]
-  (assoc-in pm [2 session-id] notifications))
+  (assoc-in pm [session-id 2] notifications))
 
 (defn add-notification!
   "Adds a notification [type value] to the list of notifications to be sent to a given session.
@@ -33,17 +33,19 @@
     aged-sessions
     (fn [pm]
       (if (contains? pm session-id)
-        (let [new-ack (+ 1 (get-ack pm session-id))
-              pm (assoc-ack pm session-id new-ack)
+        (let [last-sent (+ 1 (get-last-sent pm session-id))
+              pm (assoc-last-sent pm session-id last-sent)
               unacked-sesion-notifications (get-unacked-notifications pm session-id)
               unacked-sesion-notifications (assoc
                                              unacked-sesion-notifications
-                                             new-ack
+                                             last-sent
                                              {:notification-type type :value value})
               pm (assoc-unacked-notifications pm session-id unacked-sesion-notifications)]
           pm)
-        (add-session pm session-id (System/currentTimeMillis) 0
-                     {:notification-type type :value value})))))
+        (let [pm (add-session pm session-id (System/currentTimeMillis) 0
+                              {1 {:notification-type type :value value}})
+              pm (assoc-last-sent pm session-id 1)]
+          pm)))))
 
 (defn drop-acked-notifications
   "Remove all notifications with an ack <= last-ack"
@@ -57,9 +59,9 @@
                                              (dissoc m k)))
                                          unacked-sesion-notifications
                                          (keys unacked-sesion-notifications))
-          pm (assoc-unacked-notifications pm session-id unacked-sesion-notifications)
-          pm (assoc-ack pm session-id last-ack)]
-      pm)))
+          pm (assoc-unacked-notifications pm session-id unacked-sesion-notifications)]
+      pm)
+    pm))
 
 (defn get-session-id
   "Returns the session id (a UUID string) assigned to the current session."
